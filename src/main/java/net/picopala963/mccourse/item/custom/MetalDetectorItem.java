159cc90395dba574c7aadadc6feb91158756c08e
0xcoded <130364027+0xcoded@users.noreply.github.com>
2024-07-23 13:42:27 +0200

    Add files via upload

--- src/main/java/net/picopala963/mccourse/item/custom/MetalDetectorItem.java
+++ src/main/java/net/picopala963/mccourse/item/custom/MetalDetectorItem.java
@@ -1,15 +1,23 @@
 package net.picopala963.mccourse.item.custom;
 
+import net.picopala963.mccourse.util.ModTags;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Properties pProperties) {
@@ -17,9 +25,7 @@ public class MetalDetectorItem extends Item {
     }
 
     @Override
-    public InteractionResult useOn(UseOnContext pContext){
-
-        //Is not client side?
+    public InteractionResult useOn(UseOnContext pContext) {
         if(!pContext.getLevel().isClientSide()) {
             BlockPos positionClicked = pContext.getClickedPos();
             Player player = pContext.getPlayer();
@@ -47,30 +53,27 @@ public class MetalDetectorItem extends Item {
         return InteractionResult.SUCCESS;
     }
 
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        if(Screen.hasShiftDown()) {
+            pTooltipComponents.add(Component.translatable("tooltip.mccourse.metal_detector.tooltip.shift"));
+        } else {
+            pTooltipComponents.add(Component.translatable("tooltip.mccourse.metal_detector.tooltip"));
+        }
+
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
+
     private void outputNoValuableFound(Player player) {
         player.sendSystemMessage(Component.translatable("item.mccourse.metal_detector.no_valuables"));
     }
 
     private void outputValuableCoordinates(BlockPos below, Player player, Block block) {
-        player.sendSystemMessage(Component.literal("Value found! " + I18n.get(block.getDescriptionId()) +
-                " at ( " + below.getX() + "," + below.getY() + "," + below.getZ() + " )"));
+        player.sendSystemMessage(Component.literal("Valuable Found: " + I18n.get(block.getDescriptionId())
+                + " at (" + below.getX() + ", " + below.getY() + ", " + below.getZ() + ")"));
     }
 
-    private boolean isValuableBlock(BlockState blockState){
-        //Ores admitted
-     return blockState.is(Blocks.IRON_ORE) ||
-             blockState.is(Blocks.DEEPSLATE_IRON_ORE) ||
-             blockState.is(Blocks.COAL_ORE) ||
-             blockState.is(Blocks.DEEPSLATE_COAL_ORE) ||
-             blockState.is(Blocks.GOLD_ORE) ||
-             blockState.is(Blocks.DEEPSLATE_GOLD_ORE) ||
-             blockState.is(Blocks.DIAMOND_ORE) ||
-             blockState.is(Blocks.DEEPSLATE_DIAMOND_ORE) ||
-             blockState.is(Blocks.EMERALD_ORE) ||
-             blockState.is(Blocks.DEEPSLATE_EMERALD_ORE) ||
-             blockState.is(Blocks.COPPER_ORE) ||
-             blockState.is(Blocks.DEEPSLATE_COPPER_ORE) ||
-             blockState.is(Blocks.LAPIS_ORE) ||
-             blockState.is(Blocks.DEEPSLATE_LAPIS_ORE);
+    private boolean isValuableBlock(BlockState blockState) {
+        return blockState.is(ModTags.Blocks.METAL_DETECTOR_VALUABLES);
     }
 }
--- src/main/java/net/picopala963/mccourse/item/custom/ModFoodProperties.java
+++ src/main/java/net/picopala963/mccourse/item/custom/ModFoodProperties.java
@@ -4,8 +4,11 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
+
 public class ModFoodProperties {
     public static final FoodProperties KOHLRABI = new FoodProperties.Builder().nutrition(3).saturationMod(0.25f)
             .effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400), 0.75f).build();
+
+
 
 }
